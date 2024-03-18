f351e6de77be6407150f7c7191d9be406667be45
Serhii Chvaliuk <Serhii.Chvaliuk@centralnic.com>
2024-03-18 20:23:28 +0200

    TestLRUCacheGetFound added
    Signed-off-by: Serhii Chvaliuk <Serhii.Chvaliuk@centralnic.com>

--- lrucache/store_test.go
+++ lrucache/store_test.go
@@ -2,6 +2,7 @@ package lrucache
 
 import (
 	"testing"
+	"time"
 )
 
 func TestLRUCacheGetNotFound(t *testing.T) {
@@ -12,5 +13,31 @@ func TestLRUCacheGetNotFound(t *testing.T) {
 	}
 	if value != "" {
 		t.Errorf("got %s; want empty string", value)
+	}
+}
+
+func TestLRUCacheGetFound(t *testing.T) {
+	cache := NewLRUCache(1)
+	cache.cache["key"] = &Item{key: "key", value: "value"}
+
+	before_key_access := time.Now()
+	time.Sleep(1 * time.Millisecond)
+	value, ok := cache.Get("key")
+	time.Sleep(1 * time.Millisecond)
+	after_key_access := time.Now()
+
+	if ok != true {
+		t.Errorf("got %v; want %v", ok, true)
+	}
+	if value != "value" {
+		t.Errorf("got %s; want 'value'", value)
+	}
+
+	if !before_key_access.Before(cache.cache["key"].time) {
+		t.Errorf("Time %s has to be before %s", before_key_access, cache.cache["key"].time)
+	}
+
+	if !after_key_access.After(cache.cache["key"].time) {
+		t.Errorf("Time %s has to be after %s", after_key_access, cache.cache["key"].time)
 	}
 }
