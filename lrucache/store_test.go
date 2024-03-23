6a5f0f6fd47d0f12f5bece9adc6313a2c46cfd8d
Serhii Chvaliuk <Serhii.Chvaliuk@centralnic.com>
2024-03-23 10:06:33 +0200

    TestLRUCacheGetFound - fixed
    Signed-off-by: Serhii Chvaliuk <Serhii.Chvaliuk@centralnic.com>

--- lrucache/store_test.go
+++ lrucache/store_test.go
@@ -2,7 +2,6 @@ package lrucache
 
 import (
 	"testing"
-	"time"
 )
 
 func TestLRUCacheGetNotFound(t *testing.T) {
@@ -18,26 +17,13 @@ func TestLRUCacheGetNotFound(t *testing.T) {
 
 func TestLRUCacheGetFound(t *testing.T) {
 	cache := NewLRUCache(1)
-	cache.cache["key"] = &Item{key: "key", value: "value"}
+	cache.cache["key"] = Item{key: "key", value: "value", element: cache.queue.PushFront("key")}
 
-	before_key_access := time.Now()
-	time.Sleep(1 * time.Millisecond)
 	value, ok := cache.Get("key")
-	time.Sleep(1 * time.Millisecond)
-	after_key_access := time.Now()
-
 	if ok != true {
 		t.Errorf("got %v; want %v", ok, true)
 	}
 	if value != "value" {
 		t.Errorf("got %s; want 'value'", value)
-	}
-
-	if !before_key_access.Before(cache.cache["key"].time) {
-		t.Errorf("Time %s has to be before %s", before_key_access, cache.cache["key"].time)
-	}
-
-	if !after_key_access.After(cache.cache["key"].time) {
-		t.Errorf("Time %s has to be after %s", after_key_access, cache.cache["key"].time)
 	}
 }
