3ba8c0f040f6fbefbca6ebbd3a6b8ee908a9ae5f
Serhii Chvaliuk <Serhii.Chvaliuk@centralnic.com>
2024-03-23 10:23:44 +0200

    TestLRUCacheSetNew - added
    Signed-off-by: Serhii Chvaliuk <Serhii.Chvaliuk@centralnic.com>

--- lrucache/store_test.go
+++ lrucache/store_test.go
@@ -27,3 +27,24 @@ func TestLRUCacheGetFound(t *testing.T) {
 		t.Errorf("got %s; want 'value'", value)
 	}
 }
+
+func TestLRUCacheSetNew(t *testing.T) {
+	cache := NewLRUCache(1)
+	cache.Set("key", "value")
+	item, ok := cache.cache["key"]
+	if ok != true {
+		t.Errorf("got %v; want %v", ok, true)
+	}
+	if item.key != "key" {
+		t.Errorf("got %s; want 'key'", item.key)
+	}
+	if item.value != "value" {
+		t.Errorf("got %s; want 'value'", item.value)
+	}
+	if queueLen := cache.queue.Len(); queueLen != 1 {
+		t.Errorf("got %d; want 1", queueLen)
+	}
+	if queueValue := cache.queue.Back().Value; queueValue != "key" {
+		t.Errorf("got %s; want 'key'", queueValue)
+	}
+}
