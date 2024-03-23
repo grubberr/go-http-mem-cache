377410fc416fd07b2e619b28002a7afe3c12504c
Serhii Chvaliuk <Serhii.Chvaliuk@centralnic.com>
2024-03-23 17:19:02 +0200

    TestLRUCacheSetReplace - added
    Signed-off-by: Serhii Chvaliuk <Serhii.Chvaliuk@centralnic.com>

--- lrucache/store_test.go
+++ lrucache/store_test.go
@@ -82,3 +82,32 @@ func TestLRUCacheSetUpdate(t *testing.T) {
 		t.Errorf("got %s; want [key2 key3 key1]", keys)
 	}
 }
+
+func TestLRUCacheSetReplace(t *testing.T) {
+	cache := NewLRUCache(2)
+	cache.cache["key1"] = &Item{key: "key1", value: "value1", element: cache.queue.PushFront("key1")}
+	cache.cache["key2"] = &Item{key: "key2", value: "value2", element: cache.queue.PushFront("key2")}
+
+	cache.Set("key3", "value3")
+
+	if lenCache := len(cache.cache); lenCache != 2 {
+		t.Errorf("got %d; want 2", lenCache)
+	}
+
+	if _, ok := cache.cache["key2"]; ok != true {
+		t.Errorf("key 'key2' not found in cache")
+	}
+
+	if _, ok := cache.cache["key3"]; ok != true {
+		t.Errorf("key 'key3' not found in cache")
+	}
+
+	keys := make([]string, 0, cache.queue.Len())
+	for e := cache.queue.Back(); e != nil; e = e.Prev() {
+		keys = append(keys, e.Value.(string))
+	}
+
+	if !slices.Equal(keys, []string{"key2", "key3"}) {
+		t.Errorf("got %s; want [key2 key3]", keys)
+	}
+}
