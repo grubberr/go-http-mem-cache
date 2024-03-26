af0388df0deac045edc1b70a1e52f2bdd4a7b564
Serhii Chvaliuk <Serhii.Chvaliuk@centralnic.com>
2024-03-26 15:06:15 +0200

    CacheService added
    Signed-off-by: Serhii Chvaliuk <Serhii.Chvaliuk@centralnic.com>

--- main.go
+++ main.go
@@ -1,19 +1,63 @@
 package main
 
 import (
-	"fmt"
 	"github.com/grubberr/go-http-mem-cache/lrucache"
+	"io"
+	"net/http"
 )
 
+type CacheService struct {
+	cache *lrucache.LRUCache
+}
+
+func (s *CacheService) setCacheHandler(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseMultipartForm(10 << 20) // 10Mb
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	key := r.FormValue("key")
+	if key == "" {
+		http.Error(w, "key field is required", http.StatusBadRequest)
+		return
+	}
+
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	data, _ := io.ReadAll(file)
+	s.cache.Set(key, data)
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (s *CacheService) getCacheHandler(w http.ResponseWriter, r *http.Request) {
+	key := r.FormValue("key")
+	if key == "" {
+		http.Error(w, "key field is required", http.StatusBadRequest)
+		return
+	}
+
+	data, ok := s.cache.Get(key)
+	if !ok {
+		http.Error(w, "key not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(data)
+}
+
 func main() {
-	cache := lrucache.NewLRUCache(5)
-	cache.Set("key1", "value1")
-	cache.Set("key2", "value2")
-	cache.Set("key3", "value3")
-	cache.Set("key4", "value4")
-	cache.Set("key5", "value5")
-	cache.Get("key1")
-	cache.Set("key6", "value6")
-	fmt.Println("Cache")
-	cache.PrintCache()
+	service := CacheService{cache: lrucache.NewLRUCache(10)}
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /cache/", service.setCacheHandler)
+	mux.HandleFunc("GET /cache/", service.getCacheHandler)
+	http.ListenAndServe("localhost:8000", mux)
 }
